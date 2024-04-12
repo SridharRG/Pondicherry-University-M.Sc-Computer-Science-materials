a2c307cf317b27a40d1ac23c2fde513ed5088117
SridharRG <sridhar79.rgss@gmail.com>
2024-04-12 11:38:29 +0530

    pushed class notes

--- Semester_2/python/15-03-24.py
+++ Semester_2/python/15-03-24.py
@@ -1,58 +1,57 @@
 import keyword
+# # print(len(keyword.kwlist)) #keywords are reserved words, case-sensitive
+# print(keyword.softkwlist)
 
-print(len(keyword.kwlist)) #keywords are reserved words, case-sensitive
-print(keyword.softkwlist)
+# x = 10
+# print(bool(x))
 
-x = 10
-print(bool(x))
+# x = True
+# print(x is True)
 
-x = True
-print(x is True)
+# x = False
+# print(x is False)
 
-x = False
-print(x is False)
+# print(help("True"))
 
-print(help("True"))
+# print(keyword.iskeyword('if'))
+# print(keyword.iskeyword('range'))
 
-print(keyword.iskeyword('if'))
-print(keyword.iskeyword('range'))
+# Value keywords: True, False, None
+# x=100
+# print(bool(x))
 
-Value keywords: True, False, None
-x=100
-print(bool(x))
+# x=None
+# print(bool(x))
 
-x=None
-print(bool(x))
+# def func():
+#     print("hello") 
+#     return 10
+# x=func()
+# print(x)
 
-def func():
-    print("hello") 
-    return 10
-x=func()
-print(x)
+#Operator Keywords: and(&&), or, not, in, is(same or not =)
 
-Operator Keywords: and(&&), or, not, in, is(same or not =)
+# x=True
+# y=True
+# print(x and y)
+# print(x or y)
+# print(not x)
 
-x=True
-y=True
-print(x and y)
-print(x or y)
-print(not x)
+# x=2
+# y=2
+# print(x is y)
 
-x=2
-y=2
-print(x is y)
+# x=0
+# y=20
 
-x=0
-y=20
+# if x > 5 and y < 25:
+#     print(x+5)
 
-if x > 5 and y < 25:
-    print(x+5)
+# if x > 5 or y < 100:
+#     print(x + 5)
 
-if x > 5 or y < 100:
-    print(x + 5)
-
-if not x:
-    print(x + 5)
+# if not x:
+#     print(x + 5)
 
 
 name = "Chad"
@@ -76,8 +75,8 @@ print(x)
 # keyword cannot be used
 # cannot use symbols
 eg.
-1_num=23
-mark@=23
+#1_num=23
+#mark@=23
 
 #comments
 '''
